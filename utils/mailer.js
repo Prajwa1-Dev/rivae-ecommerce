588955539787a53b987b57ca296864509d621b18
Prajwal <prajwalmashewadi.99@gmail.com>
2026-02-11 18:51:44 +0530

    Switch email service from Gmail to Brevo SMTP

--- utils/mailer.js
+++ utils/mailer.js
@@ -34,15 +34,12 @@ if (
 // });
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: 465,
-  secure: true, // 🔥 force SSL
+  port: Number(process.env.EMAIL_PORT),
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
 });
 
 
